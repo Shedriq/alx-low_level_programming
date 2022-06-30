6cc7f095e4bb58b8789587fdf16928d020cb773a
Shedriq <jerrydwain4@gmail.com>
2022-06-30 11:24:14 +0000

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,6 +13,7 @@ int main(void)
 	long long int lli;
 	float fl;
 
+	fprintf(stderr, “”);
 	printf("Size of a char; %lu byte(s)\n", sizeof(c));
 	printf("Size of an int; %lu byte(s)\n", sizeof(i));
 	printf("Size of a long int; %lu byte(s)\n", sizeof(li));
@@ -20,4 +21,3 @@ int main(void)
 	printf("Size of a float; %lu byte(s)\n", sizeof(fl));
 	return (0);
 }
-
