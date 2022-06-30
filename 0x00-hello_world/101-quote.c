da19c3b2de404d7a772e7a311896250471508559
Shedriq <jerrydwain4@gmail.com>
2022-06-30 16:58:01 +0000

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - init
+ * Return: 1
+ */
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - \
+			Dora Korpar, 2015-10019\n", stdout);
+	return (1);
+}
