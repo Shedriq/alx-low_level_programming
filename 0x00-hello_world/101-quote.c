887f66751151a82153d0fc6f6262a09515c68234
Shedriq <jerrydwain4@gmail.com>
2022-07-01 02:22:02 +0000

    Modified

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 
 /**
- * main - init
- * Return: 1
+ * main - Entry point
+ *
+ * Return: always 1 (Success)
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+			, 59);
 	return (1);
 }
