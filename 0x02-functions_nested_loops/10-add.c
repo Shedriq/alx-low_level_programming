cf3255924204821fc1310898014941673d1c516c
Shedriq <jerrydwain4@gmail.com>
2022-07-05 20:22:28 +0000

    Return addition of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add -Entry point
+ * @n: Integer to be added
+ * @i: Integer to be added
+ * Description: This function adds two integers
+ * Return: The sum of two integers
+ */
+
+int add(int n, int i)
+{
+	int ret = n + i;
+
+	return (ret);
+}
