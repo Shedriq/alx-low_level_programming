d301c9998bba16e78d4f34ad1561d092bf22bd81
Shedriq <jerrydwain4@gmail.com>
2022-07-05 22:03:31 +0000

    104-fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - init
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	long n1 = 1;
+	long n2 = 1;
+
+	int i;
+	long sum;
+	long aux = 0;
+
+	for (i = 0; i < 50; i++)
+	{
+		if (!(i == 0))
+		{
+			sum = n1 + n2;
+			n1 = n2;
+			n2 = sum;
+			if (sum < 4000000)
+			{
+				if ((sum % 2) == 0)
+				{
+					aux = aux + sum;
+				}
+			}
+		}
+	}
+	printf("%ld", aux);
+	printf("\n");
+	return (0);
+}
