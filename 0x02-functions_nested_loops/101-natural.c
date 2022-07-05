91b709eeba824aa67ebea592737d902964a79b08
Shedriq <jerrydwain4@gmail.com>
2022-07-05 21:20:57 +0000

    101-natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - init
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int weighted_sum;
+	int counter;
+
+	for (counter = 1; counter <= 1023; counter++)
+	{
+		if ((counter % 3) == 0 || (counter % 5) == 0)
+		{
+			weighted_sum = weighted_sum + counter;
+		}
+	}
+	printf("%d", weighted_sum);
+	putchar('\n');
+	return (0);
+}
