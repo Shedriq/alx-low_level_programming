4400967d2f07131d74151b2bb341a96f778ee56c
Shedriq <jerrydwain4@gmail.com>
2022-07-26 12:48:28 +0000

    Update header file

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,7 +6,7 @@
 #include <string.h>
 
 char *create_array(unsigned int size, char c);
-char *_stfdup(char *str);
+char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int *width, int height);
 void free_grid(int **grid, int height);
