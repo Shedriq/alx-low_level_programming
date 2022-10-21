8c4055093cc6c38d798ef54d532bd61728779415
Shedriq <jerrydwain4@gmail.com>
2022-10-21 18:12:45 +0300

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,13 +1,13 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - add or update an element in a hash table
- * @ht: a pointer to the hash table
- * @key: the key to add - cannot be an empty string
- * @value: the value associated with key
+ * hash_table_set - Add or update an element in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to add - cannot be an empty string.
+ * @value: The value associated with key.
  *
- * Return: upon failure - 0
- * otherwise - 1
+ * Return: Upon failure - 0.
+ *         Otherwise - 1.
  */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
@@ -19,7 +19,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
-	value_copy = strdup(vaue);
+	value_copy = strdup(value);
 	if (value_copy == NULL)
 		return (0);
 
@@ -47,7 +47,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 	new->value = value_copy;
-	new->next = ht->array[inde];
+	new->next = ht->array[index];
 	ht->array[index] = new;
 
 	return (1);
