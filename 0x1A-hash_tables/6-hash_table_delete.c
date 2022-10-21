fcc81f72c8faffb807f1ba0604f6566a4d1feb39
Shedriq <jerrydwain4@gmail.com>
2022-10-21 20:58:49 +0300

    task 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,17 +1,16 @@
-#include "hash_tabled.h"
+#include "hash_tables.h"
 
 /**
- * hash_table_delete - Deletes a hash table
- * @ht: a pointer to a hash table
+ * hash_table_delete - Deletes a hash table.
+ * @ht: A pointer to a hash table.
  */
-
 void hash_table_delete(hash_table_t *ht)
 {
 	hash_table_t *head = ht;
 	hash_node_t *node, *tmp;
 	unsigned long int i;
 
-	for (i = 0 i < ht->size; i++)
+	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i] != NULL)
 		{
